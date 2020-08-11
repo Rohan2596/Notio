81543566785f5a6876db4f1dd86d1bcffc0833b4
BL-Rohan <rohan.kadam@bridgelabz.com>
2020-08-11 19:20:58 +0530

    ADDED:Route added to note activity from main activity.

--- app/src/main/java/com/spatalabz/notio/MainActivity.java
+++ app/src/main/java/com/spatalabz/notio/MainActivity.java
@@ -2,13 +2,27 @@ package com.spatalabz.notio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add_button=findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toNote=new Intent(MainActivity.this,NoteActivity.class);
+                startActivity(toNote);
+            }
+        });
     }
 }
